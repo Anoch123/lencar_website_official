d0d4d90582a11acac41d430e7401c676024f8099
Anoch123 <anochdissanayake@gmail.com>
2026-07-23 08:56:32 +0530

    bug fix 1

--- app/my-lencar/page.tsx
+++ app/my-lencar/page.tsx
@@ -44,7 +44,7 @@ export default function MyLencar() {
         eyebrow="Our lineup"
         heading={["Electric bikes for", "the ride ahead"]}
         description="Explore our range of smart electric vehicles designed for the modern rider."
-        imageSrc="/images/MyLencar.png"
+        imageSrc="/images/my_lencar.png"
       />
 
       <main className="bg-white text-[#0b0b0c]">
