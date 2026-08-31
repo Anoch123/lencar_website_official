6ea3e8fbd1d3615c81271102502793bbb7f05583
anoch123 <anochdissanyake@gmail.com>
2026-08-31 14:16:27 +0530

    bug fixing

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -16,6 +16,17 @@ const oswald = Oswald({
 export default function Navbar() {
     const [mobileOpen, setMobileOpen] = useState(false);
     const pathname = usePathname();
+
+    useEffect(() => {
+        if (mobileOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+        return () => {
+            document.body.style.overflow = "";
+        };
+    }, [mobileOpen]);
 
     const navItems = MENU.map(({ label, href }) => {
         const isActive = pathname === href;
